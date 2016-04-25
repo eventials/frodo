aef3af4afad2a6aad7d52eed58a22d3eb126e7c7
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2016-04-25 10:31:37 -0300

    Use pool for redis.

--- storage/storage.go
+++ storage/storage.go
@@ -3,6 +3,7 @@ package storage
 
 import (
     "log"
+    "time"
 
     "github.com/garyburd/redigo/redis"
 )
@@ -13,62 +14,95 @@ type Storage interface {
     HasKey(key string) bool
     Set(key, value string)
     Ping() bool
+    ConnectionCount() int
 }
 
 type storage struct {
-    connection redis.Conn
+    pool redis.Pool
     keyTTL int
 }
 
 func NewStorage(address string, keyTTL int) (Storage, error) {
-    c, err := redis.DialURL(address)
+    pool := redis.Pool{
+        MaxIdle: 3,
+        MaxActive: 100,
+        IdleTimeout: 120 * time.Second,
+        Dial: func () (redis.Conn, error) {
+            c, err := redis.DialURL(address)
 
-    if err != nil {
-        return nil, err
+            if err != nil {
+                return nil, err
+            }
+
+            return c, err
+        },
+        TestOnBorrow: func(c redis.Conn, t time.Time) error {
+            _, err := c.Do("PING")
+            return err
+        },
     }
 
     return &storage{
-        c,
+        pool,
         keyTTL,
     }, nil
 }
 
 func (s *storage) Close() {
-    s.connection.Close()
+    s.pool.Close()
 }
 
 func (s *storage) Get(key string) (string, error) {
     log.Printf("Getting key '%s' from cache.\n", key)
-    value, err := redis.String(s.connection.Do("GET", key))
+
+    conn := s.pool.Get()
+    defer conn.Close()
+
+    value, err := redis.String(conn.Do("GET", key))
+
     return value, err
 }
 
 func (s *storage) HasKey(key string) bool {
-    exists, _ := redis.Bool(s.connection.Do("EXISTS", key))
+    conn := s.pool.Get()
+    defer conn.Close()
+
+    exists, _ := redis.Bool(conn.Do("EXISTS", key))
+
     return exists
 }
 
 func (s *storage) Set(key, value string) {
     log.Printf("Setting key '%s' to cache.\n", key)
 
+    conn := s.pool.Get()
+    defer conn.Close()
+
     if s.keyTTL == 0 {
-        s.connection.Do("SET", key, value)
+        conn.Do("SET", key, value)
     } else {
-        s.connection.Do("SETEX", key, s.keyTTL, value)
+        conn.Do("SETEX", key, s.keyTTL, value)
     }
 }
 
 func (s *storage) Ping() bool {
-    if err := s.connection.Err(); err != nil {
+    conn := s.pool.Get()
+    defer conn.Close()
+
+    if err := conn.Err(); err != nil {
         log.Printf("Ping error: %s\n", err)
         return false
     }
 
-    value, err := redis.String(s.connection.Do("PING"))
+    value, err := redis.String(conn.Do("PING"))
 
     if err != nil {
         log.Printf("Ping error: %s\n", err)
     }
 
     return err == nil && value == "PONG"
+}
+
+func (s *storage) ConnectionCount() int {
+    return s.pool.ActiveCount()
 }
